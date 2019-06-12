e5fe76f5c93269069e50cf6b396328461f6e3bf8
Sam Ward <mail@mojzu.net>
2019-06-12 22:03:48 +0100

    Fix skeptic tests

--- manual/build.rs
+++ manual/build.rs
@@ -1,6 +1,6 @@
 use skeptic::*;
 
 fn main() {
-    let mdbook_files = markdown_files_of_directory("src/");
+    let mdbook_files = markdown_files_of_directory("md/");
     generate_doc_tests(&mdbook_files);
 }
