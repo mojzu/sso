005530faa71ff353b43f9520667e52998444654b
Sam Ward <mail@mojzu.net>
2019-06-19 22:10:48 +0100

    Test development

--- manual/src/lib2.rs
+++ manual/src/lib2.rs
@@ -1,6 +1,7 @@
-use ark_auth::client::{Client, ClientOptions, SyncClient};
+use ark_auth::client::{Client, ClientOptions, Error, RequestError, SyncClient};
 use ark_auth::core::{Key, Service, User, UserKey, UserToken};
 use ark_auth::server::route::auth::provider::Oauth2UrlResponse;
+use chrono::Utc;
 
 pub fn env_test_url() -> String {
     std::env::var("TEST_URL").unwrap()
@@ -42,8 +43,11 @@ pub fn create_user(
     active: bool,
     password: Option<&str>,
 ) -> User {
+    let before = Utc::now();
     let create = client.user_create(name, email, active, password).unwrap();
     let user = create.data;
+    assert!(user.created_at.gt(&before));
+    assert!(user.updated_at.gt(&before));
     assert!(user.id > 0);
     assert_eq!(user.name, name);
     assert_eq!(user.email, email);
@@ -51,6 +55,32 @@ pub fn create_user(
     assert!(user.password_hash.is_none());
     assert!(user.password_revision.is_none());
     user
+}
+
+pub fn create_user_duplicate_email(
+    client: &SyncClient,
+    name: &str,
+    email: &str,
+    active: bool,
+    password: Option<&str>,
+) {
+    let create = client
+        .user_create(name, email, active, password)
+        .unwrap_err();
+    assert_eq!(create, Error::Request(RequestError::BadRequest));
+}
+
+pub fn create_user_forbidden(
+    client: &SyncClient,
+    name: &str,
+    email: &str,
+    active: bool,
+    password: Option<&str>,
+) {
+    let create = client
+        .user_create(name, email, active, password)
+        .unwrap_err();
+    assert_eq!(create, Error::Request(RequestError::Forbidden));
 }
 
 pub fn create_user_key(client: &SyncClient, name: &str, service_id: i64, user_id: i64) -> UserKey {
@@ -89,12 +119,12 @@ pub fn local_login(client: &SyncClient, user_id: i64, email: &str, password: &st
     user_token
 }
 
-pub fn local_password_reset(client: &SyncClient, email: &str) -> () {
+pub fn local_password_reset(client: &SyncClient, email: &str) {
     client.auth_local_reset_password(email).unwrap()
 }
 
 pub fn microsoft_oauth2_request(client: &SyncClient) -> Oauth2UrlResponse {
     let response = client.auth_microsoft_oauth2_request().unwrap();
-    assert!(response.url.len() > 0);
+    assert!(!response.url.is_empty());
     response
 }
--- manual/tests/book.rs
+++ manual/tests/book.rs
@@ -1,10 +1,55 @@
 use manual::*;
 
+// TODO(test): Refactor tests to this format.
+
 #[test]
 fn api_ping() {
     let client = create_client();
     ping_server(&client);
 }
+
+#[test]
+fn api_user_create_ok() {
+    let mut client = create_client();
+    let (_service, service_key) = create_service_key(&client);
+    let user_email = create_user_email();
+
+    client.options.set_authorisation(&service_key.value);
+    create_user(&client, "User Name", &user_email, true, None);
+}
+
+#[test]
+fn api_user_create_bad_request_duplicate_user_email() {
+    let mut client = create_client();
+    let (_service, service_key) = create_service_key(&client);
+    let user_email = create_user_email();
+
+    client.options.set_authorisation(&service_key.value);
+    create_user(&client, "User Name", &user_email, true, None);
+
+    create_user_duplicate_email(&client, "User Name", &user_email, true, None);
+}
+
+// TODO(test): Create user bad requests.
+
+#[test]
+fn api_user_create_forbidden() {
+    let mut client = create_client();
+    let user_email = create_user_email();
+
+    client.options.set_authorisation("invalid-service-key");
+    create_user_forbidden(&client, "User Name", &user_email, true, None);
+}
+
+// #[test]
+// fn api_user_list_email_eq() {
+//     let mut client = create_client();
+//     let (service, service_key) = create_service_key(&client);
+//     let user_email = create_user_email();
+
+//     client.options.set_authorisation(&service_key.value);
+//     let user = create_user(&client, "User Name", &user_email, true, None);
+// }
 
 #[test]
 fn guide_api_key() {
