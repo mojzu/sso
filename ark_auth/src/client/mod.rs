f6c6ae9908f2a2bdd36821f186c500a0327755b5
Sam Ward <sam.ward@processinstruments.net>
2019-06-19 09:03:01 +0100

    Cargo fmt

--- ark_auth/src/client/mod.rs
+++ ark_auth/src/client/mod.rs
@@ -81,8 +81,15 @@ mod tests {
     #[test]
     fn adds_serialised_query_to_url() {
         let options = ClientOptions::new("http://localhost:9000", "authorisation-key").unwrap();
-        let query = ListQuery { gt: Some(0), lt: None, limit: Some(10) };
+        let query = ListQuery {
+            gt: Some(0),
+            lt: None,
+            limit: Some(10),
+        };
         let url = options.url_path_query("/v1/service/", &query).unwrap();
-        assert_eq!(url.to_string(), "http://localhost:9000/v1/service/?gt=0&limit=10");
+        assert_eq!(
+            url.to_string(),
+            "http://localhost:9000/v1/service/?gt=0&limit=10"
+        );
     }
 }
