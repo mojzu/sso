5fe7f26591064d0ba609df332cda18b3b63381f1
Sam Ward <sam.ward@processinstruments.net>
2019-08-05 14:45:50 +0100

    Metrics development

--- ark_auth/src/core/metrics.rs
+++ ark_auth/src/core/metrics.rs
@@ -1,11 +1,51 @@
 use crate::core::audit::AuditBuilder;
 use crate::core::{Error, Service};
 use crate::driver;
-use prometheus::{Encoder, IntCounterVec, Opts, Registry, TextEncoder};
+use prometheus::{Counter, Encoder, IntCounter, IntCounterVec, Opts, Registry, TextEncoder};
+use sysinfo::{ProcessExt, SystemExt};
 
 pub fn name(name: &str) -> String {
     let prefix = crate_name!();
     format!("{}_{}", prefix, name)
+}
+
+pub fn sysinfo_encoded() -> Result<String, Error> {
+    let registry = Registry::new();
+
+    // TODO(feature): Support more process metrics. More efficient refresh of system data.
+    // <https://prometheus.io/docs/instrumenting/writing_clientlibs/#standard-and-runtime-collectors>
+    // process_open_fds
+    // process_max_fds
+    // process_virtual_memory_bytes
+    // process_virtual_memory_max_bytes
+    // process_heap_bytes
+    // process_start_time_seconds (p.start_time)
+    let mut system = sysinfo::System::new();
+    system.refresh_all();
+    let p = system
+        .get_process(sysinfo::get_current_pid().unwrap())
+        .unwrap();
+
+    // TODO(fix): Is value in correct units.
+    let cpu_usage_counter = Counter::new(
+        "process_cpu_seconds_total",
+        "Total user and system CPU time spent in seconds.",
+    )
+    .unwrap();
+    registry
+        .register(Box::new(cpu_usage_counter.clone()))
+        .unwrap();
+    cpu_usage_counter.inc_by(p.cpu_usage() as f64);
+
+    let memory_counter = IntCounter::new(
+        "process_resident_memory_bytes",
+        "Resident memory size in bytes.",
+    )
+    .unwrap();
+    registry.register(Box::new(memory_counter.clone())).unwrap();
+    memory_counter.inc_by(p.memory() as i64);
+
+    encode_registry(&registry)
 }
 
 pub fn read(
@@ -26,9 +66,10 @@ pub fn read(
         counter.with_label_values(&[path]).inc_by(*count);
     }
 
+    let sysinfo_encoded = sysinfo_encoded()?;
     let encoded = encode_registry(registry)?;
     let audit_encoded = encode_registry(&audit_registry)?;
-    let text = format!("{}\n{}", encoded, audit_encoded);
+    let text = format!("{}\n{}\n{}", sysinfo_encoded, encoded, audit_encoded);
     Ok(text)
 }
 
---
+++ docs/ark_auth/04_changelog_0.4.0.md
@@ -0,0 +1,5 @@
+## 0.4.0 (unreleased)
+
+**Added**
+
+- Added standard metrics to Prometheus integration.
