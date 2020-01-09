466d527696ea4e496f4e8ca5d2a8472cac2c9c95
Sam Ward <sam.ward@processinstruments.net>
2020-01-09 09:29:17 +0000

    Docker development

--- docker/build/versions.sh
+++ docker/build/versions.sh
@@ -2,13 +2,23 @@
 set -e
 
 # Print versions.
+printf "os\n"
 cat /etc/os-release
+printf "\nrustup\n"
 rustup --version
+printf "\ncargo\n"
 cargo --version
+printf "\nrustc\n"
 rustc --version
+printf "\ncargo-make\n"
 cargo make --version
+printf "\ndiesel\n"
 diesel --version
+printf "\ncargo-audit\n"
 cargo audit --version
+printf "\ngo\n"
 go version
+printf "\nprotoc\n"
 protoc --version
+printf "\npandoc\n"
 pandoc --version
